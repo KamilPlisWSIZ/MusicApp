4ddbe17e119971a686c082b800f48268d1342c88
Fluttershy999YT <oliwia.summer@gmail.com>
2018-04-26 08:54:58 +0200

    Utwory tygodnia(nowa aktywnosc)

--- app/src/main/java/com/example/stud/musicapp/MainActivity.java
+++ app/src/main/java/com/example/stud/musicapp/MainActivity.java
@@ -1,10 +1,13 @@
 package com.example.stud.musicapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
+
+import com.example.stud.musicapp.topsongs.TopSongsActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,15 +16,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
         Button bTopSongs = findViewById(R.id.bTopSongs);
         bTopSongs.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Toast.makeText(MainActivity.this, "TODO", Toast.LENGTH_SHORT).show();
+            public void onClick(View v) {
 
-
+                Intent intent = new Intent(MainActivity.this, TopSongsActivity.class);
+                startActivity(intent);
             }
-        });
 
+        });
+        }
     }
-}
---
+++ app/src/main/java/com/example/stud/musicapp/topsongs/TopSongsActivity.java
@@ -0,0 +1,21 @@
+package com.example.stud.musicapp.topsongs;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.stud.musicapp.R;
+
+public class TopSongsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_top_songs);
+
+    }
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return true ;
+    }
+}
