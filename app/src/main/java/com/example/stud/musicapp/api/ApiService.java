02cb3caab619863e6531cccf2064e0075d23288d
Fluttershy999YT <oliwia.summer@gmail.com>
2018-04-26 09:18:09 +0200

    Dodanie Api

--- app/src/main/java/com/example/stud/musicapp/api/ApiService.java
+++ app/src/main/java/com/example/stud/musicapp/api/ApiService.java
@@ -9,7 +9,7 @@ import retrofit2.Retrofit;
 public class ApiService {
     public static ApiClient getService() {
         Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl("http://www.theaudiodb.com/api/v1/json/{APIKEY}/")
+                .baseUrl("http://www.theaudiodb.com/api/v1/json/1/")
                 .build();
 
         return retrofit.create(ApiClient.class);
