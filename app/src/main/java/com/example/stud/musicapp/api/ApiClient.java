98a0353d9a03f3822191d345da0f7e4268fd48ba
Fluttershy999YT <oliwia.summer@gmail.com>
2018-04-26 09:16:53 +0200

    Dodanie Api

---
+++ app/src/main/java/com/example/stud/musicapp/api/ApiClient.java
@@ -0,0 +1,8 @@
+package com.example.stud.musicapp.api;
+
+/**
+ * Created by w57449 on 26.04.2018.
+ */
+
+public class ApiClient {
+}
---
+++ app/src/main/java/com/example/stud/musicapp/api/ApiService.java
@@ -0,0 +1,18 @@
+package com.example.stud.musicapp.api;
+
+import retrofit2.Retrofit;
+
+/**
+ * Created by w57449 on 26.04.2018.
+ */
+
+public class ApiService {
+    public static ApiClient getService() {
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("http://www.theaudiodb.com/api/v1/json/{APIKEY}/")
+                .build();
+
+        return retrofit.create(ApiClient.class);
+    }
+
+}
