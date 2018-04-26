2ff2672d79d4952f10e0344e4af56c39236658bd
Fluttershy999YT <oliwia.summer@gmail.com>
2018-04-26 09:21:12 +0200

    Dodanie TrendingSingle i TrendingList

---
+++ app/src/main/java/com/example/stud/musicapp/api/TrendingList.java
@@ -0,0 +1,13 @@
+package com.example.stud.musicapp.api;
+
+import java.util.List;
+
+/**
+ * Created by w57449 on 26.04.2018.
+ */
+
+public class TrendingList {
+
+    public List<TrendingSingle> trending;
+
+}
---
+++ app/src/main/java/com/example/stud/musicapp/api/TrendingSingle.java
@@ -0,0 +1,20 @@
+package com.example.stud.musicapp.api;
+
+/**
+ * Created by w57449 on 26.04.2018.
+ */
+
+public class TrendingSingle {
+
+    public int idAlbum;
+    public int idArtist;
+    public int idTrack;
+    public int idTrend;
+    public int intChartPlace;
+    public String strAlbum;
+    public String strArtist;
+    public String strTrack;
+    public String strTrackThumb;
+
+}
+
