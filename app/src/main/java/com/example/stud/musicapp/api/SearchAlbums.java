aa59f191bf44b901cce46bbb1541c2ee0c8029f5
KamilPlisWSIZ <w57449@student.wsiz.rzeszow.pl>
2018-06-14 17:51:13 +0200

    Wyszukiwarka, pobieranie danych

---
+++ app/src/main/java/com/example/stud/musicapp/api/SearchAlbums.java
@@ -0,0 +1,11 @@
+package com.example.stud.musicapp.api;
+
+import java.util.List;
+
+/**
+ * Created by W57449 on 2018-06-14.
+ */
+
+public class SearchAlbums {
+    public List<SearchAlbum> album ;
+}
